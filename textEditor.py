f467b61d7b2910d65142b3ec12d69020a76ed894
Fl@ppY <saurav24081996@gmail.com>
2015-12-29 10:51:17 +0530

    Status Bar Update

--- textEditor.py
+++ textEditor.py
@@ -21,6 +21,11 @@ class mainWindow(QMainWindow):
         self.statusBar = self.statusBar()
         self.updateStatusBar()
 
+    def sizeHint(self):
+        height = 500
+        width = 700
+        return QSize(width, height)
+
     def updateMenuBar(self):
         self.fileMenu = self.menuBar.addMenu('&File')
         self.editMenu = self.menuBar.addMenu('&Edit')
@@ -34,11 +39,21 @@ class mainWindow(QMainWindow):
     def updateStatusBar(self):
         self.zoomSlider = QSlider(Qt.Horizontal, self.statusBar)
         self.zoomSlider.setTickPosition(QSlider.TicksAbove)
-        self.zoomSlider.setTickInterval(50)
-        self.zoomSlider.setValue(50)
-        self.curZoomValue = 50
-        self.statusBar.addWidget(self.zoomSlider)
+        self.zoomSlider.setTickInterval(10)
+        self.zoomSlider.setValue(80)
+        self.zoomSlider.setRange(70, 90)
+        self.curZoomValue = 80
+        self.countLabel = QLabel(self)
+        self.positionLabel = QLabel(self)
+        self.emptyLabel = QLabel(self)
+        self.statusBar.insertPermanentWidget(0, self.emptyLabel, 10)
+        self.statusBar.insertPermanentWidget(1, self.positionLabel, 10)
+        self.statusBar.insertPermanentWidget(2, self.countLabel, 10)
+        self.statusBar.insertPermanentWidget(3, self.zoomSlider, 4)
+        self.cursorPosition()
         self.connect(self.zoomSlider, SIGNAL("valueChanged(int)"), self.zoom)
+        self.connect(self.textArea, SIGNAL("cursorPositionChanged()"),
+                     self.cursorPosition)
 
     def zoom(self):
         value = self.zoomSlider.value()
@@ -47,6 +62,28 @@ class mainWindow(QMainWindow):
         else:
             self.textArea.zoomIn((value - self.curZoomValue))
         self.curZoomValue = value
+
+    def cursorPosition(self):
+        cursor = self.textArea.textCursor()
+        curLine = str(cursor.blockNumber() + 1)
+        curCol = str(cursor.columnNumber())
+        # #print curLine, curCol
+        self.positionLabel.setText(curLine + ',' + curCol)
+        self.wordSymbolCount()
+
+    def wordSymbolCount(self):
+        selectedText = self.textArea.textCursor().selectedText()
+        if selectedText == '':
+            text = self.textArea.toPlainText()
+            wordCount = len(text.split(' '))
+            symbolCount = len(text)
+        else:
+            wordCount = len(selectedText.split(' '))
+            symbolCount = len(selectedText)
+        # #print wordCount, symbolCount
+        self.countLabel.setText("Word: "+str(wordCount) + ", " +
+                                "Symbols: " + str(symbolCount))
+
 
 def main():
     app = QApplication(sys.argv)
